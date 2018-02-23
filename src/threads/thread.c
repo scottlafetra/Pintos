a10efa4c97e6f3407644f4c114b2ef7bd8a21b6b
Scott La Fetra <scottlafetra@gmail.com>
2018-02-23 13:09:05 -0800

    Fixed so everything but donation is working

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -213,6 +213,12 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
 
+  /* In case new thread is higher priority */
+  if( priority > thread_get_priority() )
+  {
+    thread_yield();
+  }
+
   return tid;
 }
 
@@ -347,7 +353,14 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
+  int old_priority = thread_current ()->priority;
   thread_current ()->priority = new_priority;
+  
+  /* In case we've lowered our priority */
+  if( new_priority < old_priority )
+  {
+    thread_yield();
+  }
 }
 
 /* Returns the current thread's priority. */

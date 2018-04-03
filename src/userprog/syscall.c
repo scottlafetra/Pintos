9915e52e8fb2c5cefe59da95e92319b78473ac90
Scott La Fetra <scottlafetra@gmail.com>
2018-04-02 21:07:38 -0700

    Added system call switch stuff

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -29,48 +29,126 @@ syscall_init (void)
 
 void syscall_handler (struct intr_frame * frame)
 {
-  switch( *(frame->esp) )
+  char* stack_pointer = frame->esp;
+  switch( *stack_pointer )
   {
   case SYS_HALT:
+    halt();
     break;
     
   case SYS_EXIT:
+    int status = (int) *(stack_pointer - sizeof(int));
+    exit( status );
     break;
     
   case SYS_EXEC:
+    char* cmd_line = *(stack_pointer - sizeof(char*));
+    pid_t return_pid = exec( cmd_line );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_pid;
     break;
     
   case SYS_WAIT:
+    pid_t pid = (pid_t) *(stack_pointer - sizeof(pid_t));
+    pid_t return_pid = wait( pid );
     break;
     
   case SYS_CREATE:
+    char* stack_temp = stack_pointer;
+    
+    stack_temp -= sizeof(char*);
+    char* file = *stack_temp;
+    
+    stack_temp -= sizeof(unsigned);
+    unsigned initial_size = (unsigned) *stack_temp;
+    
+    bool return_sucsess = create( file, initial_size );
+    // Return the value
+    *(frame->eax) = (uint_32) return_sucsess;
     break;
     
   case SYS_REMOVE:
+    char* file = *(stack_pointer - sizeof(char*));
+    bool return_sucsess = remove( file );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_sucsess;
     break;
     
   case SYS_OPEN:
+    char* file = *(stack_pointer - sizeof(char*));
+    int return_fd = open( file );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_fd;
+    break;
+    
+  case SYS_FILESIZE:
+    int fd = *(stack_pointer - sizeof(int));
+    int return_size = filesize( fd );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_size;
     break;
   
   case SYS_READ:
+    char* stack_temp = stack_pointer;
+    
+    stack_temp -= sizeof(char*);
+    int fd = *stack_temp;
+    
+    stack_temp -= sizeof(void*);
+    void* buffer = (void*) *stack_temp;
+    
+    stack_temp -= sizeof(unsigned);
+    unsigned size = (unsigned) *stack_temp;
+    
+    int return_value = read( fd, buffer, size );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_value;
+    
     break;
   
   case SYS_WRITE:
+    char* stack_temp = stack_pointer;
+    
+    stack_temp -= sizeof(int);
+    int fd = (int) *stack_temp;
+    
+    stack_temp -= sizeof(void*);
+    void* buffer = (void*) *stack_temp;
+    
+    stack_temp -= sizeof(unsigned);
+    unsigned size = (unsigned) *stack_temp;
+    
+    int return_value = write( fd, buffer, size );
+    
+    // Return the value
+    *(frame->eax) = (uint_32) return_value;
     break;
   
   case SYS_SEEK:
+    char* stack_temp = stack_pointer;
+    
+    stack_temp -= sizeof(int);
+    int fd = (int) *stack_temp;
+    
+    stack_temp -= sizeof(unsigned);
+    unsigned position = (unsigned) *stack_temp;
+    
+    seek( fd, position );
     break;
   
   case SYS_TELL:
+    int fd = (int) *(stack_pointer - sizeof(int));
+    tell( file );
     break;
   
   case SYS_CLOSE:
-    break;
-  
-  case SYS_HALT:
-    break;
-  
-  case SYS_HALT:
+    int fd = (int) *(stack_pointer - sizeof(int));
+    close( file );
     break;
   }
 }
